b8270315269be8fb784d0a7a6a14b16bd50cc100
TejaYeshamoni <yteja01@gmail.com>
2024-03-16 15:06:33 +0000

    Mytasks

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import Home from './components'
+
+import './App.css'
+
+// These are the lists used in the application. You can move them to any component needed.
+
+// Replace your code here
+const App = () => <Home />
+
+export default App
